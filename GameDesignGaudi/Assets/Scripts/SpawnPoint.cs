75956c55a2c1de40721612ce2d938de2e6e7f32a
EdgarNovas <edgar.novas@enti.cat>
2026-03-25 23:31:49 +0100

    Spawn When die

---
+++ GameDesignGaudi/Assets/Scripts/SpawnPoint.cs
@@ -0,0 +1,39 @@
+using UnityEngine;
+
+public class SpawnPoint : MonoBehaviour
+{
+    [SerializeField] GameObject spawnPoint;
+
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            TeleportarJugador(other.gameObject);
+        }
+    }
+
+    public void TeleportarJugador(GameObject jugador)
+    {
+        if (spawnPoint == null) 
+        {
+            Debug.LogError("Error: No has asignado el 'spawnPoint' en el inspector de la pared " + gameObject.name);
+            return;
+        }
+
+        CharacterController cc = jugador.GetComponent<CharacterController>();
+        
+        if (cc != null)
+        {
+            cc.enabled = false;
+            jugador.transform.position = spawnPoint.transform.position;
+            jugador.transform.rotation = spawnPoint.transform.rotation;
+            cc.enabled = true;
+        }
+        else
+        {
+            jugador.transform.position = spawnPoint.transform.position;
+            jugador.transform.rotation = spawnPoint.transform.rotation;
+        }
+    }
+}
